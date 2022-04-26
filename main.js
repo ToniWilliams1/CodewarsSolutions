f355ccb692413d819be1e8a3cbaedaf15081417b
Toni Williams <100317017+toniwilliams1@users.noreply.github.com>
2022-04-26 16:05:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height;
+    }
+  }
